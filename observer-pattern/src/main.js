6c9d8c0d87f365935296e9185028ff603e6698b2
Melkis Hengue <henguemelkispro@yahoo.com>
2018-10-07 10:00:04 +0200

    minor changes

--- observer-pattern/src/main.js
+++ observer-pattern/src/main.js
@@ -1,8 +1,8 @@
-import FormHandler from './slider/slider';
+import Slider from './slider/slider';
 import styles from '../assets/styles.css';
 
 // wait page load
 document.addEventListener("DOMContentLoaded", function(event) {
-  let formHandler = new FormHandler();
-  formHandler.init();
+  let slider = new Slider();
+  slider.init();
 });
