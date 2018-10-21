154cc3e1ee08d178be315561f1e66f27dc5fae9c
Melkis Hengue <henguemelkispro@yahoo.com>
2018-10-21 22:53:58 +0200

    refactoring

--- observer-pattern/src/main.js
+++ observer-pattern/src/main.js
@@ -1,6 +1,6 @@
-import Slider from './slider/slider';
 import styles from '../assets/styles.css';
 
+import Slider from './slider/slider';
 import { Square, Circle, Triangle } from './shapes/';
 
 // wait page load
@@ -12,7 +12,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // an observer
   let triangle = new Triangle({selector: '#triangle_canvas', color: 'purple'});
 
-  let slider = new Slider({selector: "#slider"});
+  // the subject
+  let slider = new Slider({selector: '#slider'});
 
   // subscribe to updates in the subject
   slider.attach(circle);
